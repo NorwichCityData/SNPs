4120935c6f7389fc3ad9c3d7fa2b006f37b1b633
Felix Shaw <felix.shaw@tgac.ac.uk>
2017-05-21 12:15:35 +0100

    additional fields setup in reportlab

--- snps/snps_proj/apps/snps/reporting/reporting2.py
+++ snps/snps_proj/apps/snps/reporting/reporting2.py
@@ -4,10 +4,11 @@ from reportlab.pdfgen import canvas
 import time
 from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.units import inch
-from reportlab.lib.colors import red
+from reportlab.lib.units import inch, cm
+from reportlab.lib import colors
+
 
 def do_report():
     doc = SimpleDocTemplate("test.pdf", pagesize=A4,
@@ -16,11 +17,39 @@ def do_report():
 
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-    styles.add(ParagraphStyle('red_title', parent=styles['title'], textColor=red))
+    styles.add(ParagraphStyle('red_title', parent=styles['title'], textColor=colors.red))
 
     story = []
     story.append(Paragraph("Case: xx704", styles["Normal"]))
     story.append(Paragraph("Test Report", styles["red_title"]))
-    print(styles["Title"])
     story.append(Paragraph("Results of DNA Analysis", styles["red_title"]))
+
+    elements = []
+
+    p = Paragraph('Nkaarco<br/> Diagnostic<br/> Testing Services.', styles['Normal'])
+
+    data = [["Case Number", "XX704"],
+
+            ["Company Name", "Nkaarco Diagnostic Testing Services."],
+            ["", "Norwich Research Park, Colney Lane, Norwich, Norfolk,"],
+            ["", "NR4 7UH"],
+
+            ["Client", "INNA U"],
+
+            ["Test", "Vitimins and Antioxidants Test"],
+
+            ["Date of receipt of items", ""]
+    ]
+    t = Table(data)
+    t.setStyle(
+        TableStyle([('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                    ('VALIGN', (0, 0), (1, -0), 'TOP'),])
+    )
+
+    # story.append(Paragraph("Case Number", styles["Normal"]))
+
+    # story.append(Spacer(height=0, width=4 * cm))
+
+    # story.append(Paragraph("Case Number", styles["Normal"]))
+    story.append(t)
     doc.build(story)
