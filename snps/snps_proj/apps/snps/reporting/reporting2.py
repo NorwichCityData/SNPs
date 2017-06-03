da68fda58c2cbce4a762d1ab86e85d6013f9f641
Felix Shaw <felix.shaw@tgac.ac.uk>
2017-06-03 13:34:41 +0100

    antioxidants and modifications sections done

--- snps/snps_proj/apps/snps/reporting/reporting2.py
+++ snps/snps_proj/apps/snps/reporting/reporting2.py
@@ -4,7 +4,7 @@ from reportlab.pdfgen import canvas
 import time
 from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER, TA_LEFT
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle, PageBreak, ListFlowable
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch, cm
 from reportlab.lib import colors
@@ -15,18 +15,37 @@ def do_report():
                             rightMargin=72, leftMargin=72,
                             topMargin=72, bottomMargin=18)
 
+    global styles
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
     styles.add(ParagraphStyle('red_title', parent=styles['title'], textColor=colors.red))
     styles.add(ParagraphStyle('red_subtitle', parent=styles['title'], textColor=colors.red, alignment=TA_LEFT))
 
+    global story
     story = []
     story.append(Paragraph("Case: xx704", styles["Normal"]))
     story.append(Paragraph("Test Report", styles["red_title"]))
     story.append(Paragraph("Results of DNA Analysis", styles["red_title"]))
 
-    elements = []
+    do_frontpage()
+    story.append(PageBreak())
 
+    do_vitamins()
+
+    do_antioxidants()
+    story.append(PageBreak())
+
+    story.append(get_title_block("Overview - Modifications to introduce"))
+
+    story.append(get_spacer())
+
+    do_mods_block("increase",
+                  ["Folic Acid", "Vitamin B6", "Vitamin e", "Vitamin B12", "Vitamin D", "Calcium", "Omega 3", "Fiber"])
+
+    doc.build(story)
+
+
+def do_frontpage():
     # do table on front page of report
     data = [["Case Number", "XX704"],
 
@@ -51,9 +70,10 @@ def do_report():
                     ('VALIGN', (0, 0), (1, -0), 'TOP'), ])
     )
     story.append(t)
-    story.append(PageBreak())
 
-    # Do copy paragraph on Vitamins and Antioxidants
+
+def do_vitamins():
+    # Do copy paragraph on Vitamins
     stitle = "Vitamins and Antioxidants"
     story.append(Paragraph(stitle, styles['red_subtitle']))
 
@@ -71,7 +91,7 @@ def do_report():
     story.append(get_spacer())
 
     # Do table for Vitamins and Antioxidants
-    data = [['Antioxidants', 'Gene', 'Variant Tested', 'Result', 'Recommended Action'],
+    data = [['Vitamin', 'Gene', 'Variant Tested', 'Result', 'Recommended Action'],
             ['Vitamin B2', 'MTHFR', 'rs1801133', 'CT', Paragraph(
                 'With your genotype, increase in homocysteine level will cause heart disease since the riboflavin level has very small effect on the homocysteine level. Healthy diet should be maintained. (1)',
                 style=styles['Normal'])],
@@ -95,12 +115,38 @@ def do_report():
     )
     story.append(t)
 
-    doc.build(story)
+
+def do_antioxidants():
+    story.append(get_spacer())
+    data = [
+        ["Antioxidants", "Gene", "Variant Tested", "Result", "Recommended Action"],
+        [Paragraph("WANKGA", styles['Normal']), 'GSTT1', 'indel', 'DD',
+         Paragraph(
+             "You have significantly reduced protection from environmental toxin. Avoid environmental pollutants such as automobile emissions and cigarette smoke which could cause skin aging and slacking.",
+             styles['Normal'])],
+        ['', 'GSTM1', 'indel', 'DI',
+         Paragraph("Your body can sufficiently defend against substances that accelerate the skin aging.",
+                   styles["Normal"])]
+    ]
+    t = Table(data, colWidths=[3 * cm, 2 * cm, 3 * cm, 2 * cm, 7 * cm])
+    t.setStyle(
+        TableStyle([('BOX', (0, 0), (-1, -1), 0.25, colors.blue),
+                    ('BACKGROUND', (0, 0), (4, 0), colors.red),
+                    ('TEXTCOLOR', (0, 0), (4, 0), colors.white),
+                    ('TEXTCOLOR', (1, 1), (1, len(data) - 1), colors.blue),
+                    ('VALIGN', (0, 0), (4, len(data) - 1), 'MIDDLE'),
+                    ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                    ('SPAN', (0, 1), (0, -1))
+                    ]),
+
+    )
+
+    story.append(t)
 
 
-def get_title_block():
+def get_title_block(header_text="Results"):
     # do results title block
-    data = [["", "Results"]]
+    data = [["", header_text]]
 
     t = Table(data, [3 * cm, 13 * cm], 1 * cm, hAlign='LEFT')
     t.setStyle(
@@ -112,6 +158,28 @@ def get_title_block():
         ])
     )
     return t
+
+
+def do_mods_block(left_text="increase", right_text=[]):
+    # do results title block
+    els = []
+    for el in right_text:
+        els.append(Paragraph(el, styles["Normal"]))
+
+    lst = ListFlowable(els)
+
+    data = [[left_text, lst]]
+
+    t = Table(data, [3 * cm, 13 * cm], hAlign='LEFT')
+    t.setStyle(
+        TableStyle([
+            ('BOX', (0, 0), (-1, -1), 0.25, colors.blue),
+            ('VALIGN', (0, 0), (0, 0), 'MIDDLE'),
+            ('TEXTCOLOR', (0, 0), (0, 0), colors.white),
+            ('BACKGROUND', (0, 0), (0, 0), colors.red)
+        ])
+    )
+    story.append(t)
 
 
 def get_spacer():
